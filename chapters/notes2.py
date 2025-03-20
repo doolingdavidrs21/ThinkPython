168781718ee3945a66ee109fc91ca3f99c5d864a
doolingdavidrs21 <david@rs21.io>
2025-03-20 14:56:53 -0600

    added chapters/notes2.py

---
+++ chapters/notes2.py
@@ -0,0 +1,36 @@
+import math
+
+n = 17
+pi = 3.14159265358979323846
+message = "And now for something completely different"
+trombones76 = "big parade"
+
+print(math.pi)
+print(math.e)
+print(math.sqrt(25))
+print(math.pow(5, 2))
+print(19 + n + round(math.pi) * 2)
+n = 17
+print("The value of pi is approximately", math.pi)
+m = 57685678  # Black will not change this numeric constant
+
+print(int('101', 2))
+print(round(math.pi, 3))
+
+x = y = 1
+print(x, y)
+r = 5 # radius of a sphere in centimeters
+
+v = (4/3)*math.pi*math.pow(r, 3) # volume of a sphere in cubic centimeters
+print(v)
+# This is a comment
+
+x = 42
+check = (math.cos(x))**2 + (math.sin(x))**2
+print(check)  # Output: 1.0
+
+# compute e**2
+print(pow(math.e, 2))  # Output: 7.38905609893065
+print(math.e**2)  # Output: 7.38905609893065
+print(math.exp(2))
+
