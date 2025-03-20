3dd51c2e68bc653c0ccf058fc934a1f84f0093b9
doolingdavidrs21 <david@rs21.io>
2025-03-20 15:04:28 -0600

    added chapters/notes.py

---
+++ chapters/notes.py
@@ -0,0 +1,21 @@
+print(round(42.5))  # Output: 42
+print(round(43.5))
+print(abs(-7))
+print(abs(-7.0))
+print(type(type))
+print(type(int))
+print(type(abs))
+minutes = 42
+seconds = 42
+total_seconds = 60 * minutes + seconds
+print(total_seconds)  # Output: 2542
+kilometers_per_mile = 1.61
+kilometers = 10
+miles = kilometers / kilometers_per_mile
+print(miles)
+# if you run a 10 kilometer race in 42 minutes and 42 seconds, what is your average pace in seconds per mile?
+average_pace_seconds_per_mile = total_seconds / miles
+print(average_pace_seconds_per_mile)  # Output:
+# what is your average speed in miles per hour?
+average_speed_miles_per_hour = miles / (total_seconds / 3600)
+print(average_speed_miles_per_hour)  # Output: 14.1
